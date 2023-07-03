8993f1e886b0cc11bae6f37ea1d18a8ea1bc853a
Abhishek Sengupta <chotuame@gmail.com>
2023-07-04 00:52:05 +0530

    done.

--- server/router/auth.js
+++ server/router/auth.js
@@ -4,12 +4,7 @@ const nodemailer = require("nodemailer");
 const cors=require("cors");
 const app=express();
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
- })
- 
+app.use(cors());
 
 require('../db/conn');
 
